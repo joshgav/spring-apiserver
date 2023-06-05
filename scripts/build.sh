b0659a4414e70f00187b242056062feaac01377b
Josh Gavant <4421720+joshgav@users.noreply.github.com>
2023-06-05 17:25:35 -0500

    add pipeline config to this repo (#6)

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,11 +5,14 @@ root_dir=$(cd ${this_dir}/.. && pwd)
 if [[ -f ${root_dir}/.env ]]; then source ${root_dir}/.env; fi
 if [[ -f ${this_dir}/.env ]]; then source ${this_dir}/.env; fi
 
+echo "this_dir: ${this_dir}"
+echo "root_dir: ${root_dir}"
+
 echo "INFO: Building application bootable jar using gradle"
 
 export GRADLE_OPTS="-Dorg.gradle.daemon=false"
 ${root_dir}/gradlew build -x test
 
 # TODO this won't work if >1 jar is generated
-echo "INFO: Copying built application to app-root..."
-cp -a ${root_dir}/build/libs/*-SNAPSHOT.jar ${ROOT_DIR}/app.jar
+echo "INFO: Copying built application to root dir..."
+cp -a ${root_dir}/build/libs/*-SNAPSHOT.jar ${root_dir}/app.jar
